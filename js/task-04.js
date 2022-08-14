a81b8bbd973c68d6cdeb095daaac067df0f7aea3
rostik1406 <98974380+rostik1406@users.noreply.github.com>
2022-08-14 17:32:14 +0300

    .

---
+++ js/task-04.js
@@ -0,0 +1,51 @@
+const CounterPlugin = function ({
+  rootSelector,
+  counterValue = 0,
+  step = 1,
+} = {}) {
+  this._value = counterValue;
+  this._step = step;
+
+  this._refs = this._getRefs(rootSelector);
+
+  this._bindEvents();
+};
+
+CounterPlugin.prototype._getRefs = function (rootSelector) {
+  const refs = {};
+  refs.container = document.querySelector(rootSelector);
+  refs.incrementBtn = refs.container.querySelector("[data-decrement]");
+  refs.decrementBtn = refs.container.querySelector("[data-increment]");
+  refs.value = refs.container.querySelector("[data-value]");
+  return refs;
+};
+
+CounterPlugin.prototype._bindEvents = function () {
+  this._refs.incrementBtn.addEventListener("click", () => {
+    console.log("CounterPlugin.prototype._bindEvents -> this", this);
+    this.increment();
+    this.updateValueUI();
+  });
+
+  this._refs.decrementBtn.addEventListener("click", () => {
+    console.log("CounterPlugin.prototype._bindEvents -> this", this);
+    this.decrement();
+    this.updateValueUI();
+  });
+};
+
+CounterPlugin.prototype.updateValueUI = function () {
+  this._refs.value.textContent = this._value;
+};
+
+CounterPlugin.prototype.increment = function () {
+  this._value += this._step;
+};
+
+CounterPlugin.prototype.decrement = function () {
+  this._value -= this._step;
+};
+
+new CounterPlugin({ rootSelector: "#counter", step: 1 });
+
+// new CounterPlugin({ rootSelector: "#counter-2", step: 1 });
