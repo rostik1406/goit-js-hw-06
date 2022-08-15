8e17f9b70295dd55f177437f535783cd637cfecb
rostik1406 <98974380+rostik1406@users.noreply.github.com>
2022-08-15 23:50:12 +0300

    .

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const inputEl = document.querySelector("#validation-input");
+console.dir(inputEl);
+
+inputEl, addEventListener("blur", onInputBlur);
+
+function onInputBlur() {
+  if (+inputEl.value.length === +inputEl.dataset.length) {
+    inputEl.classList.remove("invalid");
+    inputEl.classList.add("valid");
+  } else {
+    inputEl.classList.remove("valid");
+    inputEl.classList.add("invalid");
+  }
+}
